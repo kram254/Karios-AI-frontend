cdd71d1ec11eec3b045ccd25508189a844f6c6b3
kram254 <markorlando45@gmail.com>
2025-07-28 10:23:54 +0300

    Simple 3D Avatar Implementation

--- src/components/AnimatedAvatar.tsx
+++ src/components/AnimatedAvatar.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useMemo } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { Sphere, RoundedBox } from '@react-three/drei';
+import { Sphere } from '@react-three/drei';
 import * as THREE from 'three';
 
 interface AnimatedAvatarProps {
@@ -8,7 +8,7 @@ interface AnimatedAvatarProps {
   message: string;
 }
 
-const glyphs: Record<AnimatedAvatarProps['state'], string> = {
+const glyphs = {
   thinking: '||',
   searching: '++',
   browsing: '**',
@@ -18,15 +18,15 @@ const glyphs: Record<AnimatedAvatarProps['state'], string> = {
 };
 
 const Avatar3D: React.FC<{ state: AnimatedAvatarProps['state'] }> = ({ state }) => {
-  const groupRef = useRef<THREE.Group>(null!);
-  const matRef = useRef<THREE.MeshStandardMaterial>(null!);
+  const groupRef = useRef<THREE.Group>(null);
+  const matRef = useRef<THREE.MeshStandardMaterial>(null);
 
   const eyeTex = useMemo(() => {
     const canvas = document.createElement('canvas');
     canvas.width = canvas.height = 256;
     const ctx = canvas.getContext('2d')!;
     ctx.clearRect(0, 0, 256, 256);
-    ctx.fillStyle = '#ffffff';
+    ctx.fillStyle = '#000000';
     ctx.font = 'bold 220px/256px ui-sans-serif, system-ui';
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
@@ -52,37 +52,24 @@ const Avatar3D: React.FC<{ state: AnimatedAvatarProps['state'] }> = ({ state }) 
 
   return (
     <group ref={groupRef}>
-      <RoundedBox args={[1.2, 1.2, 1.1]} radius={0.4} smoothness={4}>
+      <Sphere args={[0.8, 32, 32]}>
         <meshStandardMaterial
           ref={matRef}
-          color="#f0f9ff"
+          color="#1d6fa5"
           metalness={0.1}
           roughness={0.25}
         />
-      </RoundedBox>
-
-      <group position={[0, 0.08, 0.56]}>
-        {[-0.22, 0.22].map((x, i) => (
-          <mesh key={i} position={[x, 0, 0]}>
-            <planeGeometry args={[0.32, 0.32]} />
-            <meshBasicMaterial map={eyeTex} transparent />
-          </mesh>
-        ))}
-      </group>
-
-      {[-0.6, 0.6].map((x, i) => (
-        <Sphere key={i} args={[0.18, 16, 16]} position={[x, -0.2, 0.45]}>
-          <meshBasicMaterial color="#ff8fab" transparent opacity={0.5} />
-        </Sphere>
-      ))}
-
-      <mesh position={[0, 0.7, 0]}>
-        <cylinderGeometry args={[0.04, 0.04, 0.4]} />
-        <meshStandardMaterial color="#c084fc" emissive="#c084fc" emissiveIntensity={0.3} />
-      </mesh>
-      <Sphere args={[0.1]} position={[0, 0.95, 0]}>
-        <meshStandardMaterial color="#c084fc" emissive="#c084fc" emissiveIntensity={0.7} />
       </Sphere>
+      <group position={[0, 0.3, 0]}>
+        <mesh position={[-0.4, 0, 0]}>
+          <planeGeometry args={[0.2, 0.2]} />
+          <meshBasicMaterial map={eyeTex} transparent />
+        </mesh>
+        <mesh position={[0.4, 0, 0]}>
+          <planeGeometry args={[0.2, 0.2]} />
+          <meshBasicMaterial map={eyeTex} transparent />
+        </mesh>
+      </group>
     </group>
   );
 };
